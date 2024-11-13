399116c267c64d18eabaf44204004290fc430bc9
akashsingh596 <72357922+akashsingh596@users.noreply.github.com>
2024-11-13 21:53:46 +0530

    update creationg BG changer

--- raectTube/src/App.jsx
+++ raectTube/src/App.jsx
@@ -1,25 +1,55 @@
+// import { useState } from "react";
+
+// function App() {
+  
+// let [counter,setcounter] = useState(0)
+
+
+// let removeCounter =()=>{
+//   setcounter(counter-1)
+// }
+
+// let addCounter =()=>{
+//   setcounter(counter+1)
+// }
+//   return (
+//     <>
+//       <h1>Hello I am AKASH SINGH</h1>
+//       <h2>Counter</h2>
+//       <h3>{counter}</h3>
+//       <button onClick={addCounter}>add</button>
+//       <button onClick={removeCounter}>remove</button>
+     
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
 import { useState } from "react";
 
+
 function App() {
-  
-let [counter,setcounter] = useState(0)
-
-
-let removeCounter =()=>{
-  setcounter(counter-1)
-}
-
-let addCounter =()=>{
-  setcounter(counter+1)
-}
+const [colour , setcolour] = useState("Olive")
   return (
     <>
-      <h1>Hello I am AKASH SINGH</h1>
-      <h2>Counter</h2>
-      <h3>{counter}</h3>
-      <button onClick={addCounter}>add</button>
-      <button onClick={removeCounter}>remove</button>
-     
+      <div
+        className="w-full h-screen duration-200"
+        style={{ background: colour }}
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div ClassName="flex flex-wrap justify-center gap-3 shadow-lg bg-white">
+            <button
+              ClassName="outline-none px-4 rounded-full"
+              style={{ background: "red" }}
+            >
+              Hello
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- raectTube/src/main.jsx
+++ raectTube/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./index.css"
 
-// import App from './App.jsx'
-import BGColourchanger from "./bGColourchanger";
-
-createRoot(document.getElementById("root")).render(<BGColourchanger />);
+import App from './App.jsx'
+createRoot(document.getElementById("root")).render(<App />);
