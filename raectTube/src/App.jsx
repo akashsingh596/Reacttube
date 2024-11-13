9b8d4e1f812ac31980af4959b67f9e6bce3e9035
akashsingh596 <72357922+akashsingh596@users.noreply.github.com>
2024-11-13 15:25:19 +0530

    learningphase1

--- raectTube/src/App.jsx
+++ raectTube/src/App.jsx
@@ -1,10 +1,26 @@
-
+import { useState } from "react";
 
 function App() {
+  
+let [counter,setcounter] = useState(0)
 
-  return (
-   <h1>Hello I am AKASH SINGH</h1>
-  )
+
+let removeCounter =()=>{
+  setcounter(counter-1)
 }
 
-export default App
+let addCounter =()=>{
+  setcounter(counter+1)
+}
+  return (
+    <>
+      <h1>Hello I am AKASH SINGH</h1>
+      <h2>Counter</h2>
+      <h3>{counter}</h3>
+      <button onClick={addCounter}>add</button>
+      <button onClick={removeCounter}>remove</button>
+    </>
+  );
+}
+
+export default App;
