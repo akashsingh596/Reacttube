92363f650f9dad83558a19683abf9dd967975607
akashsingh596 <72357922+akashsingh596@users.noreply.github.com>
2024-11-14 12:21:31 +0530

    currencyConvertor

---
+++ currencyChanger/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { UseCurrencyInfo } from './hooks/useCurrencyinfo'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl bg-orange-500'>Currency app</h1>
+      <UseCurrencyInfo/>
+    </>
+  )
+}
+
+export default App
