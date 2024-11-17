786721554b820491649e17e9006c53b16c4b6c15
akashsingh596 <72357922+akashsingh596@users.noreply.github.com>
2024-11-18 00:57:06 +0530

    FirstProject

--- contextApiProject/src/App.jsx
+++ contextApiProject/src/App.jsx
@@ -1,11 +1,16 @@
+import Login from "./Component/Login";
+import Profile from "./Component/Profile.JSX";
+import UserContextProvider from "./context/UserContextProvider";
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-     
-    </>
-  )
+    <UserContextProvider>
+      <h1>Context API App</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ contextApiProject/src/context/UserContextProvider.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import UserContext from "./userContext";
+
+const UserContextProvide = ({ children }) => {
+  const [user, setUser] = React.useState(null);
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserContextProvide;
