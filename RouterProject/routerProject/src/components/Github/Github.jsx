8400e49e4b89223b0356ea2e6be8f5d6a67084d4
akashsingh596 <72357922+akashsingh596@users.noreply.github.com>
2024-11-17 17:59:31 +0530

    CommitUpdate

--- RouterProject/routerProject/src/components/Github/Github.jsx
+++ RouterProject/routerProject/src/components/Github/Github.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
 
-const GitHub = () => {
-  return <div>hello this is Github</div>;
-};
+function Github() {
+    // const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //  fetch('https://api.github.com/users/hiteshchoudhary')
+    //  .then(response => response.json())
+    //  .then(data => {
+    //     console.log(data);
+    //     setData(data)
+    //  })
+    // }, [])
+    
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers
+    {/* <img src={data.avatar_url} alt="Git picture" width={300} /> */}
+    </div>
+  )
+}
 
-export default GitHub;
+export default Github
+
