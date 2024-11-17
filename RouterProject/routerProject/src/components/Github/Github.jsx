fb942027d8bc2b994935b5fe89ea61cd7405c1c1
akashsingh596 <72357922+akashsingh596@users.noreply.github.com>
2024-11-17 17:45:04 +0530

    PortFolio Website

---
+++ RouterProject/routerProject/src/components/Github/Github.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GitHub = () => {
+  return <div>hello this is Github</div>;
+};
+
+export default GitHub;
---
+++ RouterProject/routerProject/src/main.jsx
@@ -0,0 +1,41 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./layout.jsx";
+import Home from "./components/Home/home.jsx";
+import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+import GitHub from "./components/Github/Github.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "gitHub",
+        element: <GitHub />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
