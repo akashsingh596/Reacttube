92d35e996a83ddc45fff9656adda9aa3a9a1d1ed
akashsingh596 <72357922+akashsingh596@users.noreply.github.com>
2024-11-25 18:31:17 +0530

    project setup

---
+++ ReactToolkit/src/App/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import todoReducers from "../Features/todo/todoSlice";
+
+export const store = configureStore({
+  reducer: todoReducers,
+});
---
+++ ReactToolkit/src/components/Todos.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+
+function Todos() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Todos
